d210650c4c9628a1bc584e145b943246e2cbced8
megumi <ooo.megumi9@gmail.com>
2023-10-13 15:32:38 +0900

    アソシエーションの定義

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,7 +21,7 @@ class PicturesController < ApplicationController
 
   # POST /pictures or /pictures.json
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
 
     respond_to do |format|
       if @picture.save
@@ -55,6 +55,10 @@ class PicturesController < ApplicationController
       format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)    
   end
 
   private

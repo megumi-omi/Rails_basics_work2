b3d47044a6721318b8f2b45b43bf5c9720ce05fb
megumi <ooo.megumi9@gmail.com>
2023-10-15 12:53:48 +0900

    ユーザ新規登録機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
+
+  before_validation { email.downcase!}
+  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+  validates :password, length: {minimum: 6}
 end

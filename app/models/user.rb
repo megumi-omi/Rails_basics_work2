03b3271c745c51dd18352ff2c59ba593e55e2e3b
megumi <ooo.megumi9@gmail.com>
2023-10-17 14:57:35 +0900

    お気に入り登録機能、一覧画面追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
 
   before_validation { email.downcase!}
   validates :name, presence: true
